5ff7ee138324d36cb70afc8f601dcdb23f82909c
@theship <juburdekin@gmail.com, julee@abevy.com>
2024-04-08 16:24:38 -0700

    clean main.js

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+import Userfront from '@userfront/toolkit/vue'
+
+Userfront.init("123abc4d")
+
+// try {
+//     Userfront.init('123abc4d');
+//     console.log("Userfront initialization likely successful"); 
+// } catch (error) {
+//     console.error("Userfront initialization error:", error);
+// }
+    
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
